0ef10329441ea71eb3fde6943dd0795191ced445
mimamc95 <mimamc95@gmail.com>
2026-09-20 21:27:35 +0700

    First Commit

---
+++ python-m1/meeting1.py
@@ -0,0 +1,16 @@
+print("Hello World")      #String 
+print("Saya Berumur 30 Tahun") #Integer
+print("Saya Berkulit sawo matang") #String
+print("Saya Tinggal Di Salatiga") #String
+print("Saya Berasal Dari Jawa Tengah") #String
+
+#Variable String
+
+nama = "M.Imam Catur"    # String
+umur = 30                # Integer
+status_menikah = True    # Boolean
+tinggi = 167.0           # Float
+berat = 64.5             # Float
+
+print(f"Nama Saya adalah {nama}, umur saya yaitu {umur}, dan saya status menikah saya adalah {status_menikah}")
+print(f"Saya memiliki tinggi {tinggi} CM dan berat {berat} KG")
